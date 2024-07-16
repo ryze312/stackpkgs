3327cd9165f4e13838bc4caf47273fb1e1de5d21
Ryze <ryze@noreply.code.thishorsie.rocks>
2024-07-16 15:01:00 +0300

    Put packages under stackpkgs attribute

--- overlay.nix
+++ overlay.nix
@@ -3,8 +3,10 @@ final: prev:
 let
   stackpkgs = final.callPackage ./stackpkgs.nix;
 in
-builtins.removeAttrs stackpkgs [
-  "vscode-extensions"
-] // {
+{
+  stackpkgs = builtins.removeAttrs stackpkgs [
+    "vscode-extensions"
+  ];
+} // {
   vscode-extensions = prev.vscode-extensions // stackpkgs.vscode-extensions;
 }
