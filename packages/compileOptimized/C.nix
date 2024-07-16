29f0eb88ec7b163ef6233607dae7b0782b3f92c1
Ryze <ryze@noreply.code.thishorsie.rocks>
2024-07-16 14:29:47 +0300

    Change indentation to 2 spaces in compileOptimized.C

--- packages/compileOptimized/C.nix
+++ packages/compileOptimized/C.nix
@@ -1,10 +1,10 @@
 { lib }:
 
 {
-    optimizationLevel ? "O3",
-    enableLTO ? true,
-    enableNativeOptimizations ? true,
-    additionalFlags ? []
+	optimizationLevel ? "O3",
+	enableLTO ? true,
+	enableNativeOptimizations ? true,
+	additionalFlags ? []
 }:
 
 pkg:
@@ -12,13 +12,13 @@ pkg:
 assert builtins.elem optimizationLevel [ "O0" "O1" "O2" "O3" "Ofast" "Os" "Oz" "Og" ];
 
 let
-    flags = [ "-${optimizationLevel}" ]
-            ++ lib.optional enableLTO "-flto"
-            ++ lib.optional enableNativeOptimizations "-march=native"
-            ++ additionalFlags;
+	flags = [ "-${optimizationLevel}" ]
+			++ lib.optional enableLTO "-flto"
+			++ lib.optional enableNativeOptimizations "-march=native"
+			++ additionalFlags;
 in
 
 pkg.overrideAttrs (prevAttrs: {
-    env.NIX_CFLAGS_COMPILE = (prevAttrs.env.NIX_CFLAGS_COMPILE or "")
-                             + lib.strings.concatStringsSep " " flags;
+	env.NIX_CFLAGS_COMPILE = (prevAttrs.env.NIX_CFLAGS_COMPILE or "")
+							 + lib.strings.concatStringsSep " " flags;
 })
