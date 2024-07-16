b4303706230c449cbea85013d46c74c64b9d989f
Ryze <ryze@noreply.code.thishorsie.rocks>
2024-07-16 15:21:16 +0300

    Unwrap compileOptimized

--- packages/compileOptimized.nix
+++ packages/compileOptimized.nix
@@ -3,10 +3,8 @@
 let
   mkOverridablePackage = pkg: lib.makeOverridable (callPackage pkg {}) {};
 in
-{
-  compileOptimized = rec {
-    C = mkOverridablePackage ./compileOptimized/C.nix;
-    CNoLTO = C.override { enableLTO = false; };
-    Rust = mkOverridablePackage ./compileOptimized/Rust.nix;
-  };
+rec {
+  C = mkOverridablePackage ./compileOptimized/C.nix;
+  CNoLTO = C.override { enableLTO = false; };
+  Rust = mkOverridablePackage ./compileOptimized/Rust.nix;
 }
