9c85909c10a3dc51dd42358d466324c3e8b8c40f
Ryze <ryze@noreply.code.thishorsie.rocks>
2025-03-28 02:57:28 +0300

    Update lockfile and Invidious

--- packages/unstable/invidious.nix
+++ packages/unstable/invidious.nix
@@ -10,10 +10,10 @@ in
 # Don't build in case there is a newer version available.
 # This means we need to update commit or just use the nixpkgs provided version
 assert lib.assertMsg
-      (!(lib.strings.versionOlder version "2.20241110.0"))
+      (!(lib.strings.versionOlder version "2.20250314.0"))
       "New Invidious version (${version}), please check changelog and update";
 
 invidious-unstable.overrideAttrs {
-  # No need for the patches, since we pull from master    
+  # No need for the patches, since we pull from master
   patches = [];
 }
