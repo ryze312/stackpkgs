068a5d574ccd1cdc2171a00213daa9c4169c2e9c
Ryze <ryze@noreply.code.thishorsie.rocks>
2025-04-24 17:49:20 +0300

    [unstable/inv-sig-helper] Add version guard

--- packages/unstable/inv-sig-helper.nix
+++ packages/unstable/inv-sig-helper.nix
@@ -1,5 +1,16 @@
 {
+  lib,
   inv-sig-helper,
 }:
+
+let
+  version = inv-sig-helper.version;
+in
+
+# Don't build in case there is a newer version available.
+# This means we need to update commit or just use the nixpkgs provided version
+assert lib.assertMsg
+      (!(lib.strings.versionOlder version "0-unstable-2025-04-03"))
+      "New inv_sig-helper version (${version}), please check changelog and update";
 
 inv-sig-helper
