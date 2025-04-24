9ca1ed9320097518e8668ae8fbc068a94750c96b
Ryze <ryze@noreply.code.thishorsie.rocks>
2025-04-24 17:46:13 +0300

    [unstable/inv-sig-helper] Update to 0-unstable-2025-04-03

--- packages/unstable/inv-sig-helper.nix
+++ packages/unstable/inv-sig-helper.nix
@@ -1,16 +1,5 @@
 {
   inv-sig-helper,
-  fetchFromGitHub,
 }:
 
-inv-sig-helper.overrideAttrs (_: finalAttrs: rec {
-  name = "${finalAttrs.pname}-${version}";
-  version = "0-unstable-2025-02-26";
-
-  src = fetchFromGitHub {
-    owner = "iv-org";
-    repo = "inv_sig_helper";
-    rev = "add99d6a654bae838b030914a7fef8252406fc45";
-    hash = "sha256-p1cbXYmKBoeXF4L9XKc5jc79KW8dxzDbjZiUSMJQDs8=";
-  };
-})
+inv-sig-helper
