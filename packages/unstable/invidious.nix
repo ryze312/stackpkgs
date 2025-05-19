352044a85c3b10e9e52b5636b1c1322dca9d45b7
Ryze <ryze@noreply.code.thishorsie.rocks>
2025-05-19 06:36:17 +0300

    [packages] Update Invidious to 2.20250517.0-unstable-2025-05-17

--- packages/unstable/invidious.nix
+++ packages/unstable/invidious.nix
@@ -1,8 +1,17 @@
-{ lib, invidious }:
+{ lib, crystal, invidious }:
 
 let
-  version = invidious.version;
+  nixpkgsVersion = invidious.version;
+
+  # Hack to override shards file
+  crystalOverride = crystal // {
+    buildCrystalPackage =
+      args: crystal.buildCrystalPackage
+        (args // { shardsFile = ./invidious/shards.nix; });
+  };
+
   invidious-unstable = invidious.override {
+    crystal = crystalOverride;
     versions = lib.importJSON ./invidious/versions.json;
   };
 in
@@ -11,8 +20,8 @@ in
 # This means we need to update commit or just use the nixpkgs provided version
 # compareVersions returns 1 if left hand version is newer than the right hand version
 assert lib.assertMsg
-      (builtins.compareVersions version "2.20250517.0" != 1)
-      "New Invidious version (${version}), please check changelog and update";
+      (builtins.compareVersions nixpkgsVersion "2.20250517.0" != 1)
+      "New Invidious version (${nixpkgsVersion}), please check changelog and update";
 
 invidious-unstable.overrideAttrs {
   # No need for the patches, since we pull from master
